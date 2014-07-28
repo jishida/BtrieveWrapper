108798d51f03233799d18f3c31f4d2f458c63ead
gomi931 <gomi931.personal@gmail.com>
2014-07-28 17:25:38 +0900

    fix StringExtensions class

--- BtrieveWrapper.Orm/StringExtensions.cs
+++ BtrieveWrapper.Orm/StringExtensions.cs
@@ -7,17 +7,47 @@ namespace BtrieveWrapper.Orm
 {
     public static class StringExtensions
     {
+        static int CompareTo(this byte[] binary1, byte[] binary2) {
+            var length = binary1.Length > binary2.Length ? binary2.Length : binary1.Length;
+            for (var i = 0; i < length; i++) {
+                if (binary1[i] < binary2[i]) {
+                    return -1;
+                } else if (binary1[i] > binary2[i]) {
+                    return 1;
+                }
+                if (i == binary1.Length - 1) {
+                    if (i == binary2.Length - 1) {
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+                if (i == binary2.Length - 1) {
+                    return 1;
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
         public static bool LessThan(this string str1, string str2) {
-            return str1.CompareTo(str2) < 0;
+            var binary1 = Config.ComparedStringEncoding.GetBytes(str1);
+            var binary2 = Config.ComparedStringEncoding.GetBytes(str2);
+            return binary1.CompareTo(binary2) < 0;
         }
         public static bool LessThanOrEqual(this string str1, string str2) {
-            return str1.CompareTo(str2) < 1;
+            var binary1 = Config.ComparedStringEncoding.GetBytes(str1);
+            var binary2 = Config.ComparedStringEncoding.GetBytes(str2);
+            return binary1.CompareTo(binary2) < 1;
         }
         public static bool GreaterThan(this string str1, string str2) {
-            return str1.CompareTo(str2) > 0;
+            var binary1 = Config.ComparedStringEncoding.GetBytes(str1);
+            var binary2 = Config.ComparedStringEncoding.GetBytes(str2);
+            return binary1.CompareTo(binary2) > 0;
         }
         public static bool GreaterThanOrEqual(this string str1, string str2) {
-            return str1.CompareTo(str2) > -1;
+            var binary1 = Config.ComparedStringEncoding.GetBytes(str1);
+            var binary2 = Config.ComparedStringEncoding.GetBytes(str2);
+            return binary1.CompareTo(binary2) > -1;
         }
     }
 }
